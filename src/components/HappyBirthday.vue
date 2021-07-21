d8213706ef5d1a9b27c9f8a0f235cf001f118324
tanhuiming <m2018ming@163.com>
2021-07-21 17:04:14 +0800

    1

--- src/components/HappyBirthday.vue
+++ src/components/HappyBirthday.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="content">
     <div class="item flex flex-col flex-ai-center flex-jc-space-between">
+      <!-- 标题 -->
       <div class="title border"></div>
-      <div class="center border"></div>
+      <!-- 中间的图片轮播 -->
+      <div class="center"></div>
+      <!-- 底部 -->
+      <div class="floor"></div>
+      <!-- 呼吸灯 -->
+      <div class="Breathinglamp"></div>
     </div>
   </div>
 </template>
@@ -18,6 +24,7 @@ export default {
   min-width: 1323px;
   .item {
     height: 100vh;
+    background: #000;
     width: 100%;
     .title {
       height: 50px;
@@ -25,12 +32,15 @@ export default {
       margin-top: 100px;
     }
     .center {
+    }
+
+    .floor {
       height: 100px;
       width: 500px;
       margin-top: auto;
       margin-bottom: 50px;
       position: relative;
-      animation: flooranimation 6s ease infinite alternate;
+      // animation: flooranimation 6s ease infinite alternate;
       @keyframes flooranimation {
         from {
           left: 0px;
@@ -40,6 +50,25 @@ export default {
         }
         to {
           left: 0px;
+        }
+      }
+    }
+
+    .Breathinglamp {
+      height: 50px;
+      width: 50px;
+      border-radius: 50%;
+      background: #EEFB01;
+      animation: lamp 2s ease infinite;
+      @keyframes lamp {
+        from {
+          opacity: 0.2;
+        }
+        50% {
+          opacity: 1;
+        }
+        to {
+          opacity: 0.2;
         }
       }
     }
