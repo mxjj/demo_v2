76d67bbd17ee33a2dfe76c845aa03b226144815b
tanhuiming <m2018ming@163.com>
2021-08-24 17:59:42 +0800

    1

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,5 @@
 <template>
-	<div>
-		home123123s
-	</div>
+	<div></div>
 </template>
 <script>
 export default {
@@ -9,10 +7,36 @@ export default {
 	data() {
 		return {}
 	},
+	methods: {
+		changeArrIndex(arr, startIndex, endIndex) {
+			arr[startIndex] = arr.splice(endIndex, 1, arr[startIndex])[0]
+			return arr
+		},
+	},
 	mounted() {
 		this.$api.login().then(res => {
 			console.log(res)
 		})
+
+		let arrs = [
+			{
+				name: '小明',
+				age: '1',
+			},
+			{
+				name: '小伞',
+				age: '2',
+			},
+		]
+		for (let index in arrs) {
+			let item = arrs[index]
+			if (item.age == 2) {
+				arrs[0] = arrs.splice(index, 1, arrs[0])[0]
+				break
+			}
+		}
+
+		console.log(arrs, '============')
 	},
 }
 </script>
